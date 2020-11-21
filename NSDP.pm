30185b0d285ff84d20531a9d0e12bd997ba49eec
Konstantin S. Vishnivetsky <konstantin@sancom.lv>
2020-11-21 16:55:03 +0700

    FIX: NSDP.pm NSDP_send proc

--- NSDP.pm
+++ NSDP.pm
@@ -1,5 +1,7 @@
 package NSDP;
 
+use IO::Socket;
+
 use constant TLV_MODEL => 0x0001;
 use constant TLV_NAME => 0x0003;
 use constant TLV_MAC => 0x0004;
@@ -21,8 +23,13 @@ sub NSDP_send {
     my $selfmac = shift;
     my $swmac = shift;
     my $seq = shift;
+    my $msg = shift;
 
     my $header = "\x01\x01\x00\x00\x00\x00\x00\x00".$selfmac.$swmac."\x00\x00".pack("N", $seq)."NSDP\x00\x00\x00\x00".$msg."\xff\xff\xff\xff";
+    my $dst = sockaddr_in(63322, $DST_ADDR);
+    $sock->send($msg, 0, $dst) or die "send: $!";
+    my @hexdata = unpack("C*", $msg);
+    printf ">> %s\n", join(' ', map(sprintf("0x%.2x", $_), @hexdata));
 };
 
 1;
